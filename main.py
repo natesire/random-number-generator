48bb54ebfa443ffb81324f760282d9d387eab3da
Nate Sire <nate.sire@fairwaymc.com>
2024-01-06 00:57:54 -0500

    bump up to 10,000

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ sum = 0
 # for loop 100 times
 while(1):
     count = count + 1
-    r = random.randint(0,100)
+    r = random.randint(0,10000)
     items.append(r)
     sum = sum + r
-    if r == 99:
+    if r == 9999:
         break
 
 div = count / 2
